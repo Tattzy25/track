26c96c2b6f890ca682a0af44ab7de31abc21a259
tattzy25 <tattzy2025@gmail.com>
2026-05-26 16:41:24 -0700

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+export default function Home() {
+  return (
+    <Card className="w-full bg-[#ffffff] text-[#000000]">
+      <CardHeader>
+        <CardTitle>Project Overview</CardTitle>
+      </CardHeader>
+      <CardContent className="text-base">
+        000
+      </CardContent>
+    </Card>
+  );
+}
